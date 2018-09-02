1f312bd0f8f2195caadd44744587fa9bf41e83a7
Murillo R. Araujo <murillo_rodrigues@icloud.com>
2018-09-02 20:49:55 +0100

    Initial Commit

---
+++ Password/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Murillo R. Araujo on 2018-09-02.
+//  Copyright © 2018 Code In Capsule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
